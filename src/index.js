576f60c91f68b6e48ac083b4389fc899ce3e1c02
Prizrak <prizrak-sj@yandex.ru>
2019-09-27 09:46:59 +0300

    feat: Implement function for task Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if(str.length%2 != 0)
+    return false
+
+      let c;
+      let txt;
+      let t;
+      do
+      {
+        c = str.length;
+        bracketsConfig.forEach(function(item, i, arr) {
+          txt = str;
+          t = item[0]+item[1];
+          str = txt.replace(t,"");
+        });
+      }while (c > str.length)
+
+    if(str.length>0)
+      return false;
+    else
+      return true;
 }
